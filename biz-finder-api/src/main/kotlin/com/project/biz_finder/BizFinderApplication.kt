1dd5de4824fe9de279c788acdec8faf71a9982fb
khkil <robot625@naver.com>
2024-12-19 13:59:33 +0900

    feat: api 프로젝트 셋팅

---
+++ biz-finder-api/src/main/kotlin/com/project/biz_finder/BizFinderApplication.kt
@@ -0,0 +1,11 @@
+package com.project.biz_finder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BizFinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<BizFinderApplication>(*args)
+}
